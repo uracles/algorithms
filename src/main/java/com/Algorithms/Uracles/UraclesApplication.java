edb87f765376dff61b593ff50713e13b1f95e4fa
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-05-27 23:42:17 +0100

    new commit

--- src/main/java/com/Algorithms/Uracles/UraclesApplication.java
+++ src/main/java/com/Algorithms/Uracles/UraclesApplication.java
@@ -1,13 +1,13 @@
-package com.Algorithms;
+package com.Algorithms.Uracles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgorithmsUraclesApplication {
+public class UraclesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmsUraclesApplication.class, args);
+		SpringApplication.run(UraclesApplication.class, args);
 	}
 
 }
