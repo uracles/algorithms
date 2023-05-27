9936dd4a303338c3ae8ed147458fe3ae8bd45714
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-05-27 23:28:49 +0100

    boiler plate

---
+++ src/main/java/com/Algorithms/AlgorithmsUraclesApplication.java
@@ -0,0 +1,13 @@
+package com.Algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsUraclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsUraclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Algorithms/AlgorithmsUraclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsUraclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
