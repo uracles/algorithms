5c93ac32181a5616c2a85963b4e55f5e4fa88db3
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-06-21 13:35:04 +0100

    new challenge

---
+++ src/main/java/com/Algorithms/Uracles/AutoCorrectString.java
@@ -0,0 +1,37 @@
+package com.Algorithms.Uracles;
+
+public class AutoCorrectString {
+
+   /**
+    You've been tasked with writing an autocorrect service for messages sent by
+    your legal team. The messages which are sent to other lawyers need to be updated so
+    that each message sent references the lawyer's client for clarity. To do this you need
+    to replace all instances of "you" and its misspellings with "your client".
+
+    Complete the function so that it takes a string and replaces all
+    instances of "you", "youuu", or "u" (not case_sensitive) with "your client" (always lower case).
+
+    Return the resulting string.
+    **/
+
+
+       public static String autocorrect(String message) {
+           // Convert the message to lower case for case-insensitive matching
+           String lowerCaseMessage = message.toLowerCase();
+
+           // Replace "you" and its misspellings with "your client"
+           String correctedMessage = lowerCaseMessage.replaceAll("(?i)\\b(youuu?|u)\\b", "your client");
+
+           // Return the corrected message
+           return correctedMessage;
+
+
+       }
+
+    public static void main(String[] args) {
+        System.out.println(AutoCorrectString.autocorrect("youuuu"));
+    }
+   }
+
+
+
